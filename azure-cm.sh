2af771218c1364474ac3f580ddc350e28e345fe3
Naveen Kumar <bsainaveenkumar@gmail.com>
2022-09-27 15:55:31 +0530

    Update azure-cm.sh

--- azure-cm.sh
+++ azure-cm.sh
@@ -114,7 +114,7 @@ create_custom_role() {
   role=$1
   echo "${role}"
   az role definition create \
-    --role-definition "${role}" | jq -j '.Name'
+    --role-definition "${role}"
 } 
 
 assign_role_to_sp() {
@@ -131,7 +131,7 @@ assign_role_to_sp() {
 create_assign_role(){
 updated_role_definition=$(compose_role_definition)
 echo "${updated_role_definition}"
-role_name=$(create_custom_role "${updated_role_definition}")
+role_name=$(create_custom_role "${updated_role_definition}" |jq -j '.Name')
 echo "help2"
 echo "${role_name}"
 sleep 10s
